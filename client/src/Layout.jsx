8a1ab7752dc37509436d0d11f58b879b50db8d66
Salmapraj <salmapraz@gmail.com>
2025-11-07 10:15:08 +0545

    CA-128/todo ui

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import AppNavbar from './components/AppNavbar'
 import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
     <>
-    <AppNavbar/>
     <main>
         <Outlet/>
     </main>
