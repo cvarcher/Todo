de5e1f34d1848823deea81f0533d609ce5fbd023
Salmapraj <salmapraz@gmail.com>
2025-11-08 22:38:35 +0545

    CA-128:delete todo implemented

--- client/src/components/ListTodos.jsx
+++ client/src/components/ListTodos.jsx
@@ -3,28 +3,42 @@ import { Plus } from "lucide-react"; // icon library, optional
 import { useCompletedTodos, useGetTodos } from "../hooks/useTodos";
 import { usegetTodoById } from "../hooks/useTodos";
 import Siderbar from "./Siderbar";
+import { useDelete } from "../hooks/useTodos";
+import dele from "../assets/dele.png";
+import editing from "../assets/editing.png";
+import EditTodo from "./EditTodo";
 
-const ListTodos = ({type}) => {
 
-  const getTodosHook = useGetTodos();
-  const completedTodosHook = useCompletedTodos();
+//   const getTodosHook = useGetTodos();
+//   const completedTodosHook = useCompletedTodos();
 
-  const isType = type === "todo";
+//   const isType = type === "todo";
 
-  const tasks = isType? getTodosHook.Todos : completedTodosHook.CompletedTodos
+//   const tasks = isType? getTodosHook.Todos : completedTodosHook.CompletedTodos
 
-  const isLoading = isType? getTodosHook.isLoading: completedTodosHook.isLoading
-  const error = isType?getTodosHook.error : completedTodosHook.error
+//   const isLoading = isType? getTodosHook.isLoading: completedTodosHook.isLoading
+//   const error = isType?getTodosHook.error : completedTodosHook.error
 
-const [selectedTodoId, setSelectedTodoId] = useState(null);
-  const{TodoById,isLoading:isTodoLoading}=usegetTodoById(selectedTodoId);
+// const [selectedTodoId, setSelectedTodoId] = useState(null);
+//   const{TodoById,isLoading:isTodoLoading}=usegetTodoById(selectedTodoId);
+
+
+
+
+const ListTodos = () => {
+    const { Todos, isLoading, error } = useGetTodos();
+    const [selectedTodoId, setSelectedTodoId] = useState(null);
+    const {mutate:deleteTask} = useDelete()
+
+    const { TodoById, isLoading: isTodoLoading } =
+        usegetTodoById(selectedTodoId);
 
 
 if(isLoading) return <div>Loading...</div>;
 if(error) return <div>Error loading tasks</div>;
 
 
- if (!tasks || tasks.length === 0) {
+ if (!Todos || Todos.length === 0) {
     return (
       <div className="flex min-h-screen justify-center items-start overflow-hidden bg-gray-50">
         <Siderbar />
@@ -67,34 +81,51 @@ if(error) return <div>Error loading tasks</div>;
       {/* Header */}
       <h2 className="text-3xl font-bold mb-6 mt-10">Inbox</h2>
 
-      {/* Task List */}
-      <div className="space-y-4">
-        {tasks.map((todo) => (
-          <div key={todo._id}
-onClick={() => setSelectedTodoId(todo.task_id)}
-          className="cursor-pointer"
-          >
-            
-            <div className="flex items-start gap-3">
-              <input
-                type="radio"
-                name="task"
-                className="mt-1 w-5 h-5 text-gray-400 border-gray-300 focus:ring-0"
-              />
-              <div>
-                {todo.task_desc && (
-                  <p className="text-sm text-gray-500">{todo.task_desc}</p>
-                )}
-              </div>
-            </div>
-            <hr className="mt-3 border-gray-200" />
-          </div>
-        ))}
+            {/* Task List */}
+            <div className="space-y-4">
+                {Todos.map((todo) => (
+                    <div
+                        key={todo.task_id}
+                        onClick={() => setSelectedTodoId(todo.task_id)}
+                        className="cursor-pointer"
+                    >
+                        <div className="flex items-start gap-3">
+                            <input
+                                type="radio"
+                                name="task"
+                                className="mt-1 w-5 h-5 text-gray-400 border-gray-300 focus:ring-0"
+                            />
+                            <div className="w-full">
+                                {todo.task_desc && (
+                                    <div className="flex justify-between align-center gap-2">
+                                        <div className="text-md text-gray-600">
+                                            {todo.task_desc}
+                                        </div>
+                                        <div className="flex gap-5">
+                                          <img
+                                            src={dele}
+                                            alt="delete"
+                                            className="w-6"
+                                            onClick={(e)=>{
+                                                  e.stopPropagation(); // optional, prevent triggering parent onClick
+                                              deleteTask(todo.task_id)
+                                            }}
+                                        />
+                                    <img src={editing} alt="edit" className="w-5"  onClick={()=>{
+                                      e.stopPropagation();
+                                      <EditTodo id ={ todo.task_id}/>
+                                    }}/>
+                                        </div>
 
-        {/* Add Task Button */}
-        
-       
-      </div>
+                                    </div>
+                                )}
+                                
+                            </div>
+                        </div>
+                        <hr className="mt-3 border-gray-200" />
+                    </div>
+                ))}
+     </div>
 {/* tododescription */}
    {selectedTodoId &&(
       <div className="fixed inset-0 flex items-center justify-end mx-10 lg:justify-center">
